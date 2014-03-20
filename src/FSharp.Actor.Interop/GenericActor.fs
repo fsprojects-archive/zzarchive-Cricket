5c2c74b1344f2855e25a4f1c4e1861bd26f36265
Jesse Carter <jesse.r.carter@gmail.com>
2014-03-20 16:07:15 -0400

    C# Interop
    Simple types designed to be idiomatic to interact with from within a C#
    application

---
+++ src/FSharp.Actor.Interop/GenericActor.fs
@@ -0,0 +1,21 @@
+﻿namespace Actors.Interop
+
+open System
+open FSharp.Actor
+
+type GenericActor<'a> (name: string, computation: Action<'a>) =
+    let actor = 
+        Actor.spawn(Actor.Options.Create(name))
+            (fun (actor: IActor<'a>) ->
+                let rec loop() =
+                    async {
+                        let! (msg, sender) = actor.Receive()
+                        do computation.Invoke(msg)
+                        return! loop()
+                    }
+                loop()
+            )
+
+    member x.Actor =
+        actor
+
---
+++ src/FSharp.Actor.Interop/SynchronizedActor.fs
@@ -0,0 +1,28 @@
+﻿namespace FSharp.Actor.Interop
+
+open System
+open System.Threading
+open FSharp.Actor
+
+type SynchronizedActor<'a> (name: string, context: SynchronizationContext, computation: Action<'a>) =
+    let executeOnContext(message: 'a) = 
+        async {
+            do! Async.SwitchToContext(context)
+            computation.Invoke(message)
+        }
+    
+    let actor =
+        Actor.spawn(Actor.Options.Create(name))
+            (fun (actor: IActor<'a>) ->
+                let rec loop() =
+                    async {
+                        let! (msg, sender) = actor.Receive()
+                        do! executeOnContext(msg)
+                        return! loop()
+                    }
+                loop()
+            )
+
+    member x.Actor =
+        actor
+
