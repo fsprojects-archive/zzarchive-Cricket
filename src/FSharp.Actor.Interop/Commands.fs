8b71904265c6510aa6dff503133c1ea5704edf12
Jesse Carter <jesse.r.carter@gmail.com>
2014-03-21 12:38:49 -0400

    Interop Commands
    Continued work on a C# API that makes it more convenient to interact
    with the framework

---
+++ src/FSharp.Actor.Interop/Commands.fs
@@ -0,0 +1,27 @@
+﻿namespace FSharp.Actor.Interop
+
+open FSharp.Actor
+
+[<AutoOpen>]
+module Commands =
+    
+    let FindChildren (actorPath: string) = 
+        !* actorPath
+
+    let Find (actorPath: string) =
+        !! actorPath
+
+    let TellAll (actors: seq<IActor>) (message: 'a) =
+        actors <-* message
+
+    let Tell (actor: IActor) (message: 'a) =
+        actor <-- message
+
+    let FindAndTell (actorPath: string) (message: 'a) =
+        actorPath ?<-- message
+
+    let TellSystemMessage (actor: IActor) (systemMessage: SystemMessage) =
+        actor <!- systemMessage
+
+    let FindAndTellSystemMessage (actorPath: string) (systemMessage: SystemMessage) =
+        actorPath ?<!- systemMessage
