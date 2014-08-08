0513024ecd845192a579802721b08da51b96f8a5
Colin <colinbul@googlemail.com>
2014-08-08 15:09:35 +0100

    Updating some saved files

--- src/FSharp.Actor/AssemblyInfo.fs
+++ src/FSharp.Actor/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Actor")>]
 [<assembly: AssemblyProductAttribute("FSharp.Actor")>]
 [<assembly: AssemblyDescriptionAttribute("An actor library for F#.")>]
-[<assembly: AssemblyVersionAttribute("0.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.3")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.3"
+    let [<Literal>] Version = "0.0.4"
