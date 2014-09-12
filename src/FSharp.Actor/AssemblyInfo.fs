1fce97f41d9e441deea1ddd9f6636b17912f57bd
F# Software Foundation <fsgit@fastmail.fm>
2014-09-12 13:57:00 +0100

    update build settings for travis

--- src/FSharp.Actor/AssemblyInfo.fs
+++ src/FSharp.Actor/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Actor")>]
 [<assembly: AssemblyProductAttribute("FSharp.Actor")>]
 [<assembly: AssemblyDescriptionAttribute("An actor library for F#.")>]
-[<assembly: AssemblyVersionAttribute("0.0.5")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.5")>]
+[<assembly: AssemblyVersionAttribute("0.0.6")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.5"
+    let [<Literal>] Version = "0.0.6"
