f7d8081d7ddb05c0ada2eb2c4ed1e6acf5ac1b3c
Colin <colinbul@googlemail.com>
2014-10-10 15:39:22 +0100

    Revert "Playing trying to fix tests"
    This reverts commit e8c6bd3871127e94f6b94d5e8cde6b7a2b1983a8.

--- tests/FSharp.Actor.Tests/EventStreamTests.fs
+++ tests/FSharp.Actor.Tests/EventStreamTests.fs
@@ -11,42 +11,42 @@ type ``Given an event stream``() =
      [<Test>]
      member __.``I can post and subscribe to events``() =
         let es = new DefaultEventStream("test") :> IEventStream
-        let resultGate = new AutoResetEvent(false)
+        let resultGate = new ManualResetEventSlim(false)
         let result = ref 0
-        es.Subscribe(fun (x:int) -> result := x; resultGate.Set() |> ignore)
+        es.Subscribe(fun (x:int) -> result := x; resultGate.Set())
 
         es.Publish(10)
 
-        if resultGate.WaitOne(1000)
+        if resultGate.Wait(1000)
         then !result |> should equal 10
         else Assert.Fail("No result timeout") 
 
      [<Test>]
      member __.``I can remove a subscription post and subscribe to events by type``() =
         let es = new DefaultEventStream("test") :> IEventStream
-        let resultGate = new AutoResetEvent(false)
+        let resultGate = new ManualResetEventSlim(false)
         let result = ref 0
-        es.Subscribe(fun (x:int) -> result := x; resultGate.Set() |> ignore)
+        es.Subscribe(fun (x:int) -> result := x; resultGate.Set())
 
         es.Publish(10)
         es.Unsubscribe<int>()
         es.Publish(12)
-        if resultGate.WaitOne(1000)
+        if resultGate.Wait(1000)
         then !result |> should equal 10
         else Assert.Fail("No result timeout") 
 
      [<Test>]
      member __.``I can remove a subscription post and subscribe to events by key``() =
         let es = new DefaultEventStream("test") :> IEventStream
-        let resultGate = new AutoResetEvent(false)
+        let resultGate = new ManualResetEventSlim(false)
         let result = ref 0
-        es.Subscribe("counters", fun (x:Event) -> result := unbox<_> x.Payload; resultGate.Set() |> ignore)
+        es.Subscribe("counters", fun (x:Event) -> result := unbox<_> x.Payload; resultGate.Set())
 
         es.Publish("counters", 10)
         es.Unsubscribe("counters")
         es.Publish("counters", 12)
 
-        if resultGate.WaitOne(1000)
+        if resultGate.Wait(1000)
         then !result |> should equal 10
         else Assert.Fail("No result timeout") 
         
@@ -54,12 +54,12 @@ type ``Given an event stream``() =
      [<Test>]
      member __.``I can post and subscribe to events with an event key``() =
         let es = new DefaultEventStream("test") :> IEventStream
-        let resultGate = new AutoResetEvent(false)
+        let resultGate = new ManualResetEventSlim(false)
         let result = ref 0
-        es.Subscribe("counters", fun (x:Event) -> result := unbox<_> x.Payload; resultGate.Set() |> ignore)
+        es.Subscribe("counters", fun (x:Event) -> result := unbox<_> x.Payload; resultGate.Set())
 
         es.Publish("counters", 10)
 
-        if resultGate.WaitOne(1000)
+        if resultGate.Wait(1000)
         then !result |> should equal 10
         else Assert.Fail("No result timeout") 
