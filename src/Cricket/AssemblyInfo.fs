df694ade167b714d816dc8651c89092f114b9c2a
Colin <colinbul@googlemail.com>
2014-11-12 10:34:42 +0000

    Added missing file

---
+++ src/Cricket/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Cricket")>]
+[<assembly: AssemblyProductAttribute("Cricket")>]
+[<assembly: AssemblyDescriptionAttribute("An actor library for F#.")>]
+[<assembly: AssemblyVersionAttribute("0.0.9")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.9")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "0.0.9"
